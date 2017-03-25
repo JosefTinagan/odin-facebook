7b52a88ad65ed475b6b785f1e0889840f1cd6eea
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-03-25 20:55:29 +0800

    build FriendRequest and Friendship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :friend_requests, dependent: :destroy
+	has_many :sent_requests, through: :friend_requests, source: :friend
+	has_many :inverse_request, class_name: 'FriendRequest', foreign_key: :friend_id
+	has_many :pending_requests, through: :inverse_request, source: :user
+
+	has_many :friendships, foreign_key: :user_id, dependent: :destroy
+	has_many :active_friendships, through: :friendships, source: :friend
+	has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
+	has_many :inactive_friendships, through: :inverse_friendships, source: :user
 end
