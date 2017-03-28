d4e440a5e1d9270d8e11a0798af4fc671450e253
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-03-28 15:16:16 +0800

    made friend_request resources nested inside users resources, fixed code pertaining to the changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 
-  resources :users, only: [:index]
-  resources :friend_requests, only: [:create,:destroy]
+  resources :users, only: [:index] do
+  	resources :friend_requests, only: [:index, :create, :destroy, :show]
+  end
+  
 end
