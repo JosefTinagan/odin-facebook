0ade47747afd497c6f89c8cfdf6e56d7236d4e1c
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-04-02 20:55:09 +0800

    omniauth function working

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,16 @@ class User < ApplicationRecord
 		Post.where("author_id IN (#{friend_ids})
 							 OR author_id = :author_id", author_id: id)
 	end
+
+	def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+			user.email = auth.info.email
+			user.password = Devise.friendly_token[0,20]
+			user.name = auth.info.name # assuming the user model has a name
+			#user.image = auth.info.image # assuming the user model has an image
+			# If you are using confirmable and the provider(s) you validate you use validate emails,
+			# uncomment the line below to skip the confirmation emails
+			# user.skip_confirmation
+		end
+	end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,7 +252,8 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
-  								callback_url: "localhost:3000/users/auth/facebook/callback"
+  								callback_url: "http://localhost:3000/users/auth/facebook/callback"
+                 # ,:client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
