c9c564d55f1ed00392cb327b971021e51ebaca0f
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-04-03 21:48:21 +0800

    fix typoe of SENGRID_USERNAME and SENGRID_PASSWORD

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV.fetch('SENGRID_USERNAME'),
-    :password       => ENV.fetch('SENGRID_PASSWORD'),
+    :user_name      => ENV.fetch('SENDGRID_USERNAME'),
+    :password       => ENV.fetch('SENDGRID_PASSWORD'),
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
