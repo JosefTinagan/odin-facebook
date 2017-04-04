748ffc5124d6830720e4a9cfd50c728ca8092e8a
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-04-04 20:57:07 +0800

    Added posting picture feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,17 @@
 class PostsController < ApplicationController
   def index
   	@post = current_user.posts.build if user_signed_in?
-  	@like =current_user.likes.build if user_signed_in?
+  	@like = current_user.likes.build if user_signed_in?
   end
 
   def create
   	@post = current_user.posts.new(post_params)
   	if @post.save
   		flash[:success] = "Posted"
-  		redirect_to posts
+  		redirect_to posts_path
   	else
   		flash[:danger] = "Post unsuccessful"
+      @like = current_user.likes.build if user_signed_in?
   		render 'index'
   	end
   end
@@ -18,6 +19,6 @@ class PostsController < ApplicationController
   private
 
   	def post_params
-  		params.require(:post).permit(:content)
+  		params.require(:post).permit(:content,:picture)
   	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+
+
 	# Association with the Like model
 	has_many :likes
   has_many :liked_by, through: :likes, source: :user
@@ -12,4 +14,17 @@ class Post < ApplicationRecord
 
 	# added scope to arrange posts from newest to oldest
 	default_scope -> { order(created_at: :desc) }
+
+	# CarrierWave Gem 
+	mount_uploader :picture, PictureUploader
+
+	validate :picture_size
+	private
+
+		# Validates the size of an uploaded picture.
+		def picture_size
+			if picture.size > 5.megabytes
+				errors.add(:picture, "should be less than 5MB")
+			end
+		end
 end
